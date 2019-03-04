f12bc8a7c7a7c99bd2cf7b4b6b42ac38a0aa33c5
Jacob DeWitt <jdewitt@stu.jsu.edu>
2019-03-03 22:38:05 -0600

    Finished Feature 3

--- src/tas_fa18/Punch.java
+++ src/tas_fa18/Punch.java
@@ -20,7 +20,7 @@ public class Punch {
     private int terminalId;
     private long originalTimeStamp;
     private long adjustedTimeStamp;
-    private String typeOfAdjustment;
+    private String typeOfAdjustment = "";
 
     public Punch(Badge employeeBadges, int terminalId, int punchType, long originalTimeStamp) {
         this.employeeBadge = employeeBadges;
@@ -105,81 +105,101 @@ public class Punch {
         
         LocalTime lunchStart = s.getLunchStart();
         LocalTime lunchStop = s.getLunchStop();
-        LocalTime betweenLunchTime = lunchStart.plusMinutes(Duration.between(lunchStart, lunchStop).toMinutes());
+        System.out.println(lunchStart.getHour() + ":" + lunchStart.getMinute());
+        System.out.println(lunchStop.getHour() + ":" + lunchStop.getMinute());
+        LocalTime betweenLunchTime = lunchStart.plusMinutes((Duration.between(lunchStart, lunchStop).toMinutes())/2);
+        
+        System.out.println(betweenLunchTime.getHour() + ":" + betweenLunchTime.getMinute());
         
         int dayItIs = clockTime.get(Calendar.DAY_OF_WEEK);
         System.out.println(dayItIs);
-        
-        /*round to interval*/
-        int minuteRoundChange = thisTimeLocalTime.getMinute();
-        int moduloOfMinute = minuteRoundChange % interval;
-        long roundingToLong;
-        
-        if (moduloOfMinute >= (interval/2)) {
-            roundingToLong = new Long(interval - moduloOfMinute);
-            System.out.println("Time before: " + thisTimeLocalTime.getHour() + ":" + thisTimeLocalTime.getMinute());
-            thisTimeLocalTime = thisTimeLocalTime.plusMinutes(roundingToLong);
-            System.out.println("Time after: " + thisTimeLocalTime.getHour() + ":" + thisTimeLocalTime.getMinute());
-        } else {
-            roundingToLong = new Long(moduloOfMinute);
-            System.out.println("Time before: " + thisTimeLocalTime.getHour() + ":" + thisTimeLocalTime.getMinute());
-            thisTimeLocalTime = thisTimeLocalTime.minusMinutes(roundingToLong);
-            System.out.println("Time after: " + thisTimeLocalTime.getHour() + ":" + thisTimeLocalTime.getMinute());
-        }
-        /*Sets rounded minute*/
-        this.typeOfAdjustment = "(Interval Round)";
-        clockTime.set(Calendar.HOUR_OF_DAY, thisTimeLocalTime.get(ChronoField.HOUR_OF_DAY));
-        clockTime.set(Calendar.MINUTE, thisTimeLocalTime.get(ChronoField.MINUTE_OF_HOUR));
-        clockTime.set(Calendar.SECOND, 00);
 
         if (dayItIs != 1 && dayItIs != 7) {
             if ((thisTimeLocalTime.equals(startIntervalTime) || thisTimeLocalTime.isAfter(startIntervalTime)) && thisTimeLocalTime.isBefore(start)) {
                 clockTime.set(Calendar.HOUR_OF_DAY, start.get(ChronoField.HOUR_OF_DAY));
                 clockTime.set(Calendar.MINUTE, start.get(ChronoField.MINUTE_OF_HOUR));
                 clockTime.set(Calendar.SECOND, start.get(ChronoField.SECOND_OF_MINUTE));
+                thisTimeLocalTime = start;
                 this.typeOfAdjustment = "(Shift Start)";
             } else if (thisTimeLocalTime.equals(start)){
                 clockTime.set(Calendar.HOUR_OF_DAY, start.get(ChronoField.HOUR_OF_DAY));
                 clockTime.set(Calendar.MINUTE, start.get(ChronoField.MINUTE_OF_HOUR));
                 clockTime.set(Calendar.SECOND, start.get(ChronoField.SECOND_OF_MINUTE));
+                thisTimeLocalTime = start;
                 this.typeOfAdjustment = "(Shift Start)";
-            } else if (thisTimeLocalTime.isAfter(start) && thisTimeLocalTime.isBefore(startGraceTime)) {
+            } else if (thisTimeLocalTime.isAfter(start) && (thisTimeLocalTime.isBefore(startGraceTime) || thisTimeLocalTime.equals(startGraceTime))) {
                 clockTime.set(Calendar.HOUR_OF_DAY, start.get(ChronoField.HOUR_OF_DAY));
                 clockTime.set(Calendar.MINUTE, start.get(ChronoField.MINUTE_OF_HOUR));
                 clockTime.set(Calendar.SECOND, start.get(ChronoField.SECOND_OF_MINUTE));
-                this.typeOfAdjustment = "(None2)";
-            } else if (thisTimeLocalTime.isAfter(startGraceTime) && thisTimeLocalTime.isBefore(startDockTime)){
+                thisTimeLocalTime = start;
+                this.typeOfAdjustment = "(Shift Start)";
+            } else if (thisTimeLocalTime.isAfter(startGraceTime) && (thisTimeLocalTime.isBefore(startDockTime) || thisTimeLocalTime.equals(startDockTime))) {
                 clockTime.set(Calendar.HOUR_OF_DAY, startDockTime.get(ChronoField.HOUR_OF_DAY));
                 clockTime.set(Calendar.MINUTE, startDockTime.get(ChronoField.MINUTE_OF_HOUR));
                 clockTime.set(Calendar.SECOND, startDockTime.get(ChronoField.SECOND_OF_MINUTE));
-                
-            } else if (thisTimeLocalTime.isAfter(stopDockTime) && thisTimeLocalTime.isBefore(stopGraceTime)) {
+                thisTimeLocalTime = startDockTime;
+                this.typeOfAdjustment = "(Shift Dock)";
+            } else if ((thisTimeLocalTime.isAfter(stopDockTime) || thisTimeLocalTime.equals(stopDockTime)) && thisTimeLocalTime.isBefore(stopGraceTime)) {
                 clockTime.set(Calendar.HOUR_OF_DAY, stopDockTime.get(ChronoField.HOUR_OF_DAY));
                 clockTime.set(Calendar.MINUTE, stopDockTime.get(ChronoField.MINUTE_OF_HOUR));
                 clockTime.set(Calendar.SECOND, stopDockTime.get(ChronoField.SECOND_OF_MINUTE));
-                                
-            } else if (thisTimeLocalTime.isAfter(stopGraceTime) && thisTimeLocalTime.isBefore(stop)) {
+                thisTimeLocalTime = stopDockTime;
+                this.typeOfAdjustment = "(Shift Dock)";                   
+            } else if (thisTimeLocalTime.isAfter(stopGraceTime) && (thisTimeLocalTime.isBefore(stop) || thisTimeLocalTime.equals(stop))) {
                 clockTime.set(Calendar.HOUR_OF_DAY, stop.get(ChronoField.HOUR_OF_DAY));
                 clockTime.set(Calendar.MINUTE, stop.get(ChronoField.MINUTE_OF_HOUR));
                 clockTime.set(Calendar.SECOND, stop.get(ChronoField.SECOND_OF_MINUTE));
-
+                thisTimeLocalTime = stop;
+                this.typeOfAdjustment = "(Shift Stop)";
             } else if (thisTimeLocalTime.isAfter(stop) && thisTimeLocalTime.isBefore(stopIntervalTime)){
                 clockTime.set(Calendar.HOUR_OF_DAY, stop.get(ChronoField.HOUR_OF_DAY));
                 clockTime.set(Calendar.MINUTE, stop.get(ChronoField.MINUTE_OF_HOUR));
                 clockTime.set(Calendar.SECOND, stop.get(ChronoField.SECOND_OF_MINUTE));
-
+                thisTimeLocalTime = stop;
+                this.typeOfAdjustment = "(Shift Stop)";
             } else if ((thisTimeLocalTime.equals(lunchStart)|| thisTimeLocalTime.isAfter(lunchStart)) && thisTimeLocalTime.isBefore(betweenLunchTime)) {
+                System.out.println("lunchStart");
                 clockTime.set(Calendar.HOUR_OF_DAY, lunchStart.get(ChronoField.HOUR_OF_DAY));
                 clockTime.set(Calendar.MINUTE, lunchStart.get(ChronoField.MINUTE_OF_HOUR));
                 clockTime.set(Calendar.SECOND, lunchStart.get(ChronoField.SECOND_OF_MINUTE));
+                thisTimeLocalTime = lunchStart;
                 this.typeOfAdjustment = "(Lunch Start)";
             } else if (thisTimeLocalTime.isAfter(betweenLunchTime) && (thisTimeLocalTime.isBefore(lunchStop) || thisTimeLocalTime.equals(lunchStop))) {
                 clockTime.set(Calendar.HOUR_OF_DAY, lunchStop.get(ChronoField.HOUR_OF_DAY));
                 clockTime.set(Calendar.MINUTE, lunchStop.get(ChronoField.MINUTE_OF_HOUR));
                 clockTime.set(Calendar.SECOND, lunchStop.get(ChronoField.SECOND_OF_MINUTE));
+                thisTimeLocalTime = lunchStop;
                 this.typeOfAdjustment = "(Lunch Stop)";
             }
         }
+        
+        /*round to interval*/
+        int minuteRoundChange = thisTimeLocalTime.getMinute();
+        int moduloOfMinute = minuteRoundChange % interval;
+        long roundingToLong;
+        if (moduloOfMinute != 0) {
+            if (moduloOfMinute >= (interval/2)) {
+                roundingToLong = new Long(interval - moduloOfMinute);
+                System.out.println("Time before: " + thisTimeLocalTime.getHour() + ":" + thisTimeLocalTime.getMinute());
+                thisTimeLocalTime = thisTimeLocalTime.plusMinutes(roundingToLong);
+                System.out.println("Time after: " + thisTimeLocalTime.getHour() + ":" + thisTimeLocalTime.getMinute());
+            } else {
+                roundingToLong = new Long(moduloOfMinute);
+                System.out.println("Time before: " + thisTimeLocalTime.getHour() + ":" + thisTimeLocalTime.getMinute());
+                thisTimeLocalTime = thisTimeLocalTime.minusMinutes(roundingToLong);
+                System.out.println("Time after: " + thisTimeLocalTime.getHour() + ":" + thisTimeLocalTime.getMinute());
+            }
+            
+            /*Sets rounded minute*/
+            this.typeOfAdjustment = "(Interval Round)";
+            clockTime.set(Calendar.HOUR_OF_DAY, thisTimeLocalTime.get(ChronoField.HOUR_OF_DAY));
+            clockTime.set(Calendar.MINUTE, thisTimeLocalTime.get(ChronoField.MINUTE_OF_HOUR));
+            clockTime.set(Calendar.SECOND, 00);
+        } else if (moduloOfMinute == 0 && this.typeOfAdjustment.equals("")) {
+            clockTime.set(Calendar.SECOND, 00);
+            this.typeOfAdjustment = "(None)";
+        }
+        
         /*
         if (dayItIs != 1 && dayItIs != 7) {
             
